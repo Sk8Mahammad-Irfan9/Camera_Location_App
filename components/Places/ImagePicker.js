8365c3d47a6ab71c18a92b89e85b4468cacf882c
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-08-09 20:49:10 +0530

    First commit

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,79 @@
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState, useEffect } from "react";
+import { Colors } from "../../constanst/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
+function ImagePicker({ onTakeImage }) {
+  const [pickedImage, setPickedImage] = useState();
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permission to use this app."
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.assets[0].uri);
+    onTakeImage(image.assets[0].uri);
+  };
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    height: 200,
+    width: "100%",
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
---
+++ util/location.js
@@ -0,0 +1,20 @@
+
+
+export function getMapPreview(lat, lng) {
+  const imagePreviewUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${GEOAPIFY_API_KEY}&format=jpeg&center=lonlat:${lng},${lat}&marker=lonlat:${lng},${lat};color:%23ff0000;size:medium`;
+
+  return imagePreviewUrl;
+}
+
+export async function getAddress(lat, lng) {
+  const response = await fetch(
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&format=json&apiKey=${GEOAPIFY_API_KEY}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch address!");
+  }
+  const data = await response.json();
+  const address = await data.results[0].formatted;
+  console.log(address);
+  return address;
+}
